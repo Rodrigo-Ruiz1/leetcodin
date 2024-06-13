8acd0d0a3879cafae9c7878a7e931f9766cfe55c
Rodrigo <ruiz.rodrigo46@yahoo.com>
2024-06-13 13:38:38 -0500

    first tree problem

---
+++ Trees/maximumDepthOfABinaryTree.js
@@ -0,0 +1,8 @@
+function maxDepth(root) {
+    if (root == null) {
+        return 0
+    }
+    let leftDepth = maxDepth(root.left)
+    let rightDepth = maxDepth(root.right)
+    return Math.max(leftDepth, rightDepth) + 1
+}
